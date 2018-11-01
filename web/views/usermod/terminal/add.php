007d6f4f3d7962f6c8d7d0062596ca25266939cf
lijianeng10 <1028617248@qq.com>
2018-11-01 10:42:35 +0800

    1

--- web/views/usermod/terminal/add.php
+++ web/views/usermod/terminal/add.php
@@ -5,23 +5,4 @@
             <input type="text" class="easyui-numberbox" name="nums" id="nums" data-options="required:true">
         </div>
     </form>
-    <style>
-        #qrcode {
-            width: 150px;
-            height:150px;
-        }
-    </style>
-    <div id="qrcode"></div>
 </div>
-<script>
-
-    function createQRcode() {
-        var qrcode = new QRCode(document.getElementById("qrcode"), {
-            width: $('#qrcode').width(),
-            height: $('#qrcode').height()
-        });
-        var elText = 'http://114.115.148.102:8011/store/store/to-jump-page?terminalNum=PS100001'
-        qrcode.makeCode(elText)
-    }
-    createQRcode()
-</script>
