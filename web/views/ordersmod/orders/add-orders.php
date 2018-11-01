58cbadc9c9a71641602f2d3316a74c831fd1241d
lsr <452692259@qq.com>
2018-11-01 16:03:19 +0800

    orders

--- web/views/ordersmod/orders/add-orders.php
+++ web/views/ordersmod/orders/add-orders.php
@@ -26,7 +26,7 @@
         </div>
         <div class="form-item" style="width:80%">
             <label for="" class="label-top">网点编号：</label>
-            <input type="text" name="cust_no" class="easyui-textbox" prompt="请输入网点编号" style="width:60%">
+            <input type="text" name="cust_no_new" class="easyui-textbox" prompt="请输入网点编号" style="width:60%">
         </div>
         <div class="form-item" style="width:80%">
             <label for="" class="label-top" style="font-size:16px;font-weight: bold">彩票信息</label>
@@ -91,26 +91,24 @@
 		toAdd();
 		function toAdd(){
 			var count = parseFloat($("#count").val());
-			for(var i=1;i<=count;i++){
 				
-				//彩种下拉框
-				$('#lottery_id' + count).combobox({
-					url:'/ordersmod/orders/get-lottery',
-					valueField:'id',
-					textField:'text',
-					panelHeight:'auto',
-					editable:false,//不可编辑，只能选择
-				});
-				//面值下拉框
-				$('#sub_value' + count).combobox({
-					url:'/ordersmod/orders/get-sub-value',
-					valueField:'id',
-					textField:'text',
-					panelHeight:'auto',
-					editable:false,//不可编辑，只能选择
-				});
-				
-			}
+			//彩种下拉框
+			$('#lottery_id' + count).combobox({
+				url:'/ordersmod/orders/get-lottery',
+				valueField:'id',
+				textField:'text',
+				panelHeight:'auto',
+				editable:false,//不可编辑，只能选择
+			});
+			//面值下拉框
+			$('#sub_value' + count).combobox({
+				url:'/ordersmod/orders/get-sub-value',
+				valueField:'id',
+				textField:'text',
+				panelHeight:'auto',
+				editable:false,//不可编辑，只能选择
+			});
+			
 		}
 			
 		//新增彩票订单
@@ -225,7 +223,7 @@
             });
             return false;
         }*/
-		var cust_no = $("input[name='cust_no']").val();
+		var cust_no = $("input[name='cust_no_new']").val();
 		if (!cust_no) {
             $.messager.show({
                 title: '提示',
