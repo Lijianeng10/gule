c6fa7d5ef989d6dd2512e5e2d023dfb938567378
zlylnn <280997242@qq.com>
2018-11-01 09:56:44 +0800

    STORE

---
+++ modules/store/store.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\store;
+
+/**
+ * tools module definition class
+ */
+class store extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\store\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
