ce16a82cf7d98ac85914e9a977b34cb3eacfe4a8
lijianeng10 <1028617248@qq.com>
2019-01-03 15:10:12 +0800

    1

--- modules/front/controllers/LoginController.php
+++ modules/front/controllers/LoginController.php
@@ -14,25 +14,26 @@ class LoginController extends Controller {
      * @return type
      */
     public function actionIndex() {
-//        $agent = $_SERVER['HTTP_USER_AGENT'];
-        echo 1111;die;
-        $request = \Yii::$app->request;
-        $custNo = $request->get('myCustNo', '');
-        $statusBarHeight = $request->get('statusBarHeight', '');
-        if (!$custNo) {
-            if (strpos($_SERVER['HTTP_USER_AGENT'], 'AlipayClient') === false) {
-                //非支付宝 返回错误提示
-                $url = \Yii::$app->params['userDomain'] . '/h5_ggc/error.html?msg=请使用支付宝客户端扫码购彩' . '&statusBarHeight=' . $statusBarHeight;
-                return $this->redirect($url);
+            $signature = $_GET['signature'] ;
+            $timestamp = $_GET['timestamp'] ;
+            $nonce = $_GET['nonce'] ;
+            $echostr = $_GET['echostr'] ;
+            $token = 'weixin' ;
+
+            $arr = array($token,$timestamp,$nonce) ;
+            //自然排序
+            sort($arr) ;
+            //拆成字符串 加密
+            $str = sha1(implode($arr)) ;
+            /*
+                第一次配置个人服务器时候 为新服务器会传递参数$echostr
+                与个人服务器建立连接之后  不会传递该参数
+            */
+            if($str == $signature && $echostr) {
+                echo $echostr ;
+                die ;
             }
         }
-        $terminalNum = $request->get('terminalNum', '');
-        if (empty($terminalNum)) {
-            $url = \Yii::$app->params['userDomain'] . '/h5_ggc/error.html?msg=参数缺失' . '&statusBarHeight=' . $statusBarHeight;
-            return $this->redirect($url);
-        }
-        $ret = StoreService::toJumpPage($custNo, $terminalNum, $statusBarHeight);
-        return $this->redirect($ret);
-    }
+
 
 }
