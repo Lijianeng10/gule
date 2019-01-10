70fee74354c5ed66fa1759be1fff4dcaa776065b
lijianeng10 <1028617248@qq.com>
2019-01-10 18:03:18 +0800

    1

--- modules/front/controllers/UserController.php
+++ modules/front/controllers/UserController.php
@@ -66,5 +66,21 @@ class UserController extends Controller {
         $token = UserService::autoLogin($user['cust_no'], $user['user_id']); //自动登录
         return $this->jsonResult(600, '登录成功', ['token' => $token, 'cust_no' => $user['cust_no']]);
     }
+    /**
+     * 说明: 获取用户个人信息
+     * @return
+     */
+    public function actionGetUserDetail(){
+        $request = \Yii::$app->request;
+        $custNo = $request->post_nn('custNo');
+//        $fieds = ['user.user_name', 'user.user_tel', 'user.cust_no', 'user.user_pic', 'user.province', 'user.city', 'user.area', 'user.address', 'user.authen_status', 'uf.all_funds', 'uf.able_funds', 'uf.ice_funds', 'uf.no_withdraw','uf.pay_password has_pwd'];
+        $user = User::find()
+//            ->select($fieds)
+//            ->leftJoin('user_funds as uf', 'uf.cust_no = user.cust_no')
+            ->where(['user.cust_no' => $custNo])
+            ->asArray()
+            ->one();
+        return $this->jsonResult(600, '获取成功', $user);
+    }
 
 }
