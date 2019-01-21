35c5cab4f04856125efef4df849bd31613627275
lijianeng10 <1028617248@qq.com>
2019-01-21 17:21:19 +0800

    1

--- modules/tools/helpers/AliSms.php
+++ modules/tools/helpers/AliSms.php
@@ -92,7 +92,7 @@ class AliSms{
         $request->setPhoneNumbers($phone);
 
         // 必填，设置签名名称，应严格按"签名名称"填写，请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/sign
-        $request->setSignName("谷乐数据");
+        $request->setSignName("阿里云短信测试专用");
 
         // 必填，设置模板CODE，应严格按"模板CODE"填写, 请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/template
         $request->setTemplateCode($moduleCode);
