1cb9e116614d76a2c09dbea4ce96d3351ab07db5
lijianeng10 <1028617248@qq.com>
2019-02-22 16:46:48 +0800

    1

--- config/params_test.php
+++ config/params_test.php
@@ -6,4 +6,9 @@ return [
     'lottery_img_host' => 'http://119.23.239.189:8082', //图片服务器地址
     'wx_appid'=>'wxe287ca0a504d7126',//微信公众号APPID
     'wx_app_secret'=>'28dfa0301d30b26feef3bd550996d8ff',//微信公众号APPSECRET
+
+    'gl_shop_appid' => 'wx774016baa223f8f6',//咕啦商城小程序appid
+    'gl_shop_mch_id' => '1514825831',//咕啦商城商户ID
+    'gl_shop_secret' => '77d196072921adfd9656bbb94e72a717',//咕啦商城 app secret
+    'gl_shop_pay_key' => '0257c8b69d6f9a49285a82a8b7942636',//咕啦商城支付秘钥
 ];
