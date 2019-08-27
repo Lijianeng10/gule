bbff004ed420bdcc71643ff95ce98a7ef544e849
lijianeng10 <1028617248@qq.com>
2019-08-27 09:42:49 +0800

    1

--- config/params_test.php
+++ config/params_test.php
@@ -1,9 +1,7 @@
 <?php
 return [
-    'userDomain' => "http://119.23.239.189:1011", //前台域名
-    'java_appId' => '362a00a28e234199a0d911cbdd1d4c67', //钱包支付AppId
-    'java_appKey' => 'edd3c3cfb1d147f89124c5f5f969f272e601f61599ef49679a038ceaab36355d' , // 钱包支付AppKey
-    'lottery_img_host' => 'http://119.23.239.189:8082', //图片服务器地址
+    'userDomain' => "http://www.gulezyun.com.cn", //前台域名
+    'lottery_img_host' => 'http://sys.gulezyun.com.cn/img', //图片服务器地址
 
     'gl_shop_appid' => 'wx774016baa223f8f6',//咕啦商城小程序appid
     'gl_shop_mch_id' => '1514825831',//咕啦商城商户ID
