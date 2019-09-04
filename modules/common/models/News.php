ab30d581829e8f4d5e0f8c8eba1c2b63bd7a00f8
lijianeng10 <1028617248@qq.com>
2019-09-04 10:11:14 +0800

    1

---
+++ modules/common/models/News.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace app\modules\common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "news".
+ *
+ * @property integer $news_id
+ * @property string $pic_name
+ * @property string $pic_url
+ * @property string $pc_pic_url
+ * @property integer $jump_type
+ * @property string $jump_title
+ * @property string $jump_url
+ * @property string $content
+ * @property string $sub_content
+ * @property integer $area
+ * @property integer $type
+ * @property integer $info_type
+ * @property integer $use_type
+ * @property string $league_code
+ * @property integer $status
+ * @property string $create_time
+ * @property string $update_time
+ * @property integer $opt_id
+ * @property integer $praise_num
+ * @property integer $comment_num
+ */
+class News extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'news';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['jump_type', 'area', 'type', 'info_type', 'use_type', 'status', 'opt_id', 'praise_num', 'comment_num'], 'integer'],
+            [['content', 'sub_content'], 'string'],
+            [['create_time'], 'required'],
+            [['create_time', 'update_time'], 'safe'],
+            [['pic_name', 'jump_title'], 'string', 'max' => 50],
+            [['pic_url', 'pc_pic_url', 'jump_url'], 'string', 'max' => 255],
+            [['league_code'], 'string', 'max' => 25],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'news_id' => 'News ID',
+            'pic_name' => 'Pic Name',
+            'pic_url' => 'Pic Url',
+            'pc_pic_url' => 'Pc Pic Url',
+            'jump_type' => 'Jump Type',
+            'jump_title' => 'Jump Title',
+            'jump_url' => 'Jump Url',
+            'content' => 'Content',
+            'sub_content' => 'Sub Content',
+            'area' => 'Area',
+            'type' => 'Type',
+            'info_type' => 'Info Type',
+            'use_type' => 'Use Type',
+            'league_code' => 'League Code',
+            'status' => 'Status',
+            'create_time' => 'Create Time',
+            'update_time' => 'Update Time',
+            'opt_id' => 'Opt ID',
+            'praise_num' => 'Praise Num',
+            'comment_num' => 'Comment Num',
+        ];
+    }
+}
---
+++ modules/front/controllers/DataController.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/09/04
+ * Time: 10:03:46
+ */
+namespace  app\modules\front\controllers;
+
+use Yii;
+use yii\web\Controller;
+use app\modules\common\models\News;
+
+class DataController extends Controller{
+    /**
+     * 赛事资讯数据接入
+     * @return json
+     */
+    public function actionInformationData() {
+        $request = \Yii::$app->request;
+        $json = $request->post('data', '');
+        $data = json_decode($json, true);
+        $news = new News();
+        $news->pic_name = $this->arrParameter($data[0], 'pic_name');
+        $news->pic_url = $this->arrParameter($data[0], 'pic_url');
+        $news->content = $this->arrParameter($data[0], 'content');
+        $news->type = $this->arrParameter($data[0], 'type');
+//        $news->info_type = $this->arrParameter($data[0], 'info_type');
+//        $news->league_code = $this->arrParameter($data[0], 'league_code');
+        $news->create_time = $this->arrParameter($data[0], 'create_time');
+        $news->sub_content = $this->arrParameter($data[0], 'sub_content');
+        $news->status = 1;
+        if ($news->validate()) {
+            if ($news->save()) {
+                return $this->jsonResult(600, '数据写入成功', true);
+            }
+            return $this->jsonResult(109, '数据写入失败', $news->errors);
+        } else {
+            return $this->jsonResult(109, '数据验证失败', $news->errors);
+        }
+    }
+    /**
+     * 判断数组是否存在该键
+     * @param type $data
+     * @param type $name
+     * @return type
+     */
+    public function arrParameter($data, $name) {
+        if (!isset($data[$name])) {
+            return $this->jsonError(109, $name . "参数缺失;");
+        }
+        return $data[$name];
+    }
+}
